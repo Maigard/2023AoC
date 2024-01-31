6a71ce631e4be5b30859b6bd461e533ff01cb461
Ken Schmidt <kenschmidt@gmail.com>
2024-01-31 13:49:42 -0800

    Not sure this is more legible 🤷‍♂️

--- day1/part2.py
+++ day1/part2.py
@@ -5,11 +5,14 @@ import re
 def main():
     lines = []
     keys = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-            "six": 6, "seven": 7, "eight": 8, "nine": 9}
+            "six": 6, "seven": 7, "eight": 8, "nine": 9,
+            "0": 0, "1": 1, "2": 2, "3": 3, "4": 4,
+            "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+
     with open(sys.argv[1], "r") as f:
         for line in f:
-            line = re.findall(r"(?=(0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine))", line)
-            line = [keys[i] if i in keys else int(i) for i in line]
+            line = re.findall(f"(?=({'|'.join(keys.keys())}))", line)
+            line = [keys[i] for i in line]
             lines.append(line[0] * 10  + line[-1])
     print(sum(lines))
 
