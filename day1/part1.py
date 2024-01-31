ba459a20bc4646b3580ea473c2ba15dba4e94d17
Ken Schmidt <kenschmidt@gmail.com>
2024-01-31 13:25:17 -0800

    Use better number filter in  day1/part1.py

--- day1/part1.py
+++ day1/part1.py
@@ -5,7 +5,7 @@ def main():
     numbers = []
     with open(sys.argv[1], "r") as f:
         for line in f:
-            line = "".join(i for i in line if i in "0123456789")
+            line = "".join(i for i in line if i.isdecimal())
             numbers.append(int(line[0] + line[-1]))
     print(sum(numbers))
 
